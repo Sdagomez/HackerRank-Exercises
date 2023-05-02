96dbcabdda55f804c92f0aada334eaebdb9e1e89
Sergio <sdagomez7048@gmail.com>
2023-05-01 19:42:43 -0500

    Fix

--- dia1.py
+++ dia1.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         elif n >=6 and n <=20:
             print('Weird')
         elif n > 20:
-            print('Not Weird')
+            print('Not Weird!')
 
     else:
         print('Weird')
